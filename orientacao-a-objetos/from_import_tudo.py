f6bb227bf006092735cb2b4d8ea2990ca02f3cec
Gislaine Reis <gmvbr.2002@gmail.com>
2024-02-06 08:55:58 -0300

    excluindo partes de orientação a objetos para criação de novo repositório

--- orientacao-a-objetos/from_import_tudo.py
+++
@@ -1,7 +0,0 @@
-from funcoes_do_log import * #o * é para importar tudo (mas não é uma boa prática)
-# tudo o que tem nesse namespace é importado para cá
-# não é uma boa prática pois nem sempre é usado TUDO de outro módulo
-
-imprimir_no_log(f"Bem vinda, {nome_de_usuario}")
-# não foi necessário colocar o funcoes_do_log. pois ele já importou tudo
-#ou seja não precisa mais definir
