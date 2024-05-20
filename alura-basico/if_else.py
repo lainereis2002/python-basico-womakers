b4626d53671591fb894dfa2d48d11577c8b30ae9
Gislaine Reis <gmvbr.2002@gmail.com>
2024-05-20 13:58:47 -0300

    alura aula 02 if_else

---
+++ alura-basico/if_else.py
@@ -0,0 +1,36 @@
+import os
+
+def exibir_nome_do_programa():
+    print('Sabor Express\n')
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_programa():
+    os.system('cls') #limpar terminal no windows
+    #os.system('clear') #limpar terminal no linux
+    print('Finalizando o programa')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'Você escolheu a opção {opcao_escolhida}')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_programa()
+    
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+    
+if __name__ == '__main__':
+    main()
