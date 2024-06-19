cad4afe86caa58c9399ed814c40e63ce7a082c8c
Gislaine Reis <gmvbr.2002@gmail.com>
2024-06-19 03:45:26 -0300

    atualizando

--- alura-basico/if_else.py
+++ alura-basico/if_else.py
@@ -10,7 +10,7 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado do restaurante')
     print('4. Sair\n')
 
 def finalizar_programa():
@@ -29,7 +29,10 @@ def opcao_invalida():
     
 def exibir_subtitulo(texto):
     os.system("cls")
+    linha = '*' * (len(texto))
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def cadastrar_novo_restaurante():
@@ -47,11 +50,12 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo("Listando os restaurantes")
     
+    print(f"{'Nome do Restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status")
     for restaurante in restaurantes:
         nome_restaurante = restaurante["nome"]
         categoria_restaurante = restaurante["categoria"]
-        status_restaurante = restaurante["ativo"]
-        print(f"- {nome_restaurante} | {categoria_restaurante} | {status_restaurante}")
+        ativo = "ativado" if restaurante["ativo"] else "desativado"
+        print(f"- {nome_restaurante.ljust(20)} | {categoria_restaurante.ljust(20)} | {ativo}")
     
     voltar_ao_menu()
 
